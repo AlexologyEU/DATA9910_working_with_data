86584c84b3332ec17b11c89906b262a5ad75a927
AlexologyEU <alex.murphy2@gmail.com>
2021-10-09 15:08:38 +0100

    r practice using datacamp

--- test_r_file.R
+++ test_r_file.R
@@ -1,1 +1,12 @@
-print("Test of commit")
+#print("Test of commit")
+
+#matrix(1:9, byrow = TRUE, nrow = 3, )
+
+
+new_hope <- c(460.998, 314.4)
+empire_strikes <- c(290.475, 247.900)
+return_jedi <- c(309.306, 165.8)
+
+box_office <- c(new_hope, empire_strikes, return_jedi)
+sw_matrix <- matrix(box_office, byrow = TRUE, nrow = 3)
+sw_matrix
